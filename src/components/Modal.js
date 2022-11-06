cad92ca2d2406032fc51c9ed1f80b82970bbbd37
DostonSuhrobov <doston.sanatovich@gmail.com>
2022-11-06 13:50:14 -0500

    new compoenents

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,8 +2,8 @@ function Modal(){
     return (
     <div>
         <p>Are you sure?</p>
-        <button>cancel</button>
-        <button>confirm</button>
+        <button className="btn btn--alt">cancel</button>
+        <button className="btn">confirm</button>
     </div>
  )
 }
