bea540b7b58b956bd8a0795797723e51539e23ac
DostonSuhrobov <doston.sanatovich@gmail.com>
2022-11-06 13:50:03 -0500

    files added

---
+++ src/components/Modal.js
@@ -0,0 +1,11 @@
+function Modal(){
+    return (
+    <div>
+        <p>Are you sure?</p>
+        <button>cancel</button>
+        <button>confirm</button>
+    </div>
+ )
+}
+
+export default Modal;
---
+++ src/components/Todo.js
@@ -0,0 +1,16 @@
+ function Todo(props){
+    function clicked_e(){
+        console.log('Hey ' + props.text + ' clicked!')
+    }
+
+    return (   
+        <div className="card">
+            <h2>{props.text}</h2>
+            <div className="actions">
+            <button className="btn" onClick={clicked_e}>Delete</button>
+            </div>
+        </div>
+    )
+ }
+
+ export default Todo;
