4035f448504c09c7e1f2156072fed5a0a1458b48
DostonSuhrobov <doston.sanatovich@gmail.com>
2022-11-07 06:25:00 -0500

    new app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
- import Backdrop from './components/Backdrop';
- import Modal from './components/Modal';
- import Todo from './components/Todo'
+
+
 
 
 function App() {
   return <div>
-    <h1>My Todos</h1>
-    <Todo text = 'Learn ReactJS' /> 
-    <Todo text = 'Educate yourself' /> 
-    <Todo text = 'Embrace pain and difficulties' /> 
 
-    <Modal />
-    <Backdrop />
+
+
     
   </div>;
 }
