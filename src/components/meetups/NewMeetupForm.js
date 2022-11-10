466372b34fac311e41837b536721158fb35c9aba
DostonSuhrobov <doston.sanatovich@gmail.com>
2022-11-10 17:58:15 -0500

    form changes

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -2,8 +2,11 @@ import Card from '../ui/Card'
 import classes from './NewMeetupForm.module.css'
 
 function NewMeetupForm(){
+
+    function submitHandler(){}
+
     return <Card>
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={submitHandler}>
             <div className={classes.control}>
                 <label htmlFor='title'>Meetup Title</label>
                 <input type='text' required id="title" />
